20b9d77ec6bbfddcb2c0f088491e4e9b40dc8001
Kate Belinskaya <kate.belinskaya@gmail.com>
2018-12-01 12:50:38 -0500

    Removed build folder from the repository

--- build/AppVersion.build/Release-iphoneos/AppVersion.build/DerivedSources/AppVersion_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char AppVersionVersionString[];
- extern const double AppVersionVersionNumber;
-
- const unsigned char AppVersionVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:AppVersion  PROJECT:AppVersion-1" "\n";
- const double AppVersionVersionNumber __attribute__ ((used)) = (double)1.;
--- build/AppVersion.build/Release-iphoneos/AppVersion.build/Objects-normal/armv7/AppVersion_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/melonamin/Library/Mobile\ Documents/com~apple~CloudDocs/Projects/amebalabs/AppVersion/build/AppVersion.build/Release-iphoneos/AppVersion.build/DerivedSources/AppVersion_vers.c
